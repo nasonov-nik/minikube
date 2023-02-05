d5835502f25fe45a5be5061bc3ab8d2272e21707
nikita <nasonov.nik@gmail.com>
2023-02-05 16:57:50 +0300

    test jenkins

--- start.groovy
+++ start.groovy
@@ -6,9 +6,10 @@ pipeline {
     stages {
         stage('test'){
             steps {
+                def my_host String
                 //sh 'kubectl get deployment'
                 sh 'pwd'
-                //def my_host = sh(script: 'ls -l', returnStdout: true)
+                my_host = sh(script: 'pwd', returnStdout: true)
                 //println("host: ${my_host}")
             }
         }
