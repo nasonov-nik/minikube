298590c9eaaac86c12a0f4b117faeee38923160e
nikita <nasonov.nik@gmail.com>
2023-02-05 16:01:56 +0300

    test jenkins

--- start.groovy
+++ start.groovy
@@ -9,7 +9,7 @@ pipeline {
                 //sh 'kubectl get deployment'
                 sh 'ls -la'
                 sh 'pwd'
-                host = sh (script: 'hostname -i',returnStdout: true)
+                host = sh (script: "hostname -i", returnStdout: true)
                 println("host: ${host}")
             }
         }
