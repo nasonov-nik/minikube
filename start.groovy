4aa0e8d64f2d81de6e8cda441c2b3ec7072a9a2a
nikita <nasonov.nik@gmail.com>
2023-02-05 16:06:16 +0300

    K

--- start.groovy
+++ start.groovy
@@ -7,9 +7,8 @@ pipeline {
         stage('test'){
             steps {
                 //sh 'kubectl get deployment'
-                sh 'ls -la'
                 sh 'pwd'
-                def host = sh (script: "hostname -i", returnStdout: true)
+                def my_host = sh(script: "ls -l", returnStdout: true)
                 println("host: ${host}")
             }
         }
